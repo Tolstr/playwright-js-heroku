530a535b72ceab7afed4eb99980b6ed4cc76e216
Anatolii Strilets <strilets89@gmail.com>
2025-07-02 16:51:24 -0700

    Added 1 test with empty credentials and 1 test with Valid username and invalid password

--- tests/login/login.spec.js
+++ tests/login/login.spec.js
@@ -2,12 +2,12 @@ import {test, expect} from '@playwright/test';
 
 const baseURL = 'https://the-internet.herokuapp.com';
 
-test.describe('Login Tests', () => {
+test.describe('Login Flow', () => {
     test.beforeEach(async ({page}) => {
         await page.goto(baseURL + '/login');
     });
 
-    test('User should be able to login and logout successfully', async ({page}) => {
+    test('Valid login', async ({page}) => {
         await page.getByLabel('username').fill('tomsmith');
         await page.getByLabel('password').fill('SuperSecretPassword!');
         await page.locator('button[type="submit"]').click();
@@ -19,12 +19,28 @@ test.describe('Login Tests', () => {
         await expect(page.locator('#flash')).toBeVisible();
     });
 
-    test.only('User should not be able to login with invalid credentials', async ({page}) => {
+    test.only('Invalid login and invalid password', async ({page}) => {
         await page.getByLabel('username').fill('invalidUser');
         await page.getByLabel('password').fill('invalidPassword');
         await page.locator('button[type="submit"]').click();
 
         await expect(page.locator('#flash')).toBeVisible();
         await expect(page.locator('#flash')).toContainText('Your username is invalid!');
+    });
+
+    test ('Empty credentials', async ({page}) => {
+        await page.locator('button[type="submit"]').click();
+
+        await expect(page.locator('#flash')).toBeVisible();
+        await expect(page.locator('#flash')).toContainText('Your username is invalid!');
+    });
+
+    test.only('Valid username and wrong password', async ({page}) => {
+        await page.getByLabel('username').fill('tomsmith');
+        await page.getByLabel('password').fill('WrongPassword!');
+        await page.locator('button[type="submit"]').click();
+
+        await expect(page.locator('#flash')).toBeVisible();
+        await expect(page.locator('#flash')).toContainText('Your password is invalid!');
     });
 });
